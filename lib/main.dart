84e8d2924abeee39655ad3a58b5ae5ca8287ce18
OmidTaheri <m.omidtaheri@yahoo.com>
2020-04-09 14:38:08 +0430

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:fluttermoviedetails/movie_details_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        accentColor: Color(0XFFFF5959)
+      ),
+      home: MovieDetailsPage(),
+    );
+  }
+}
+
+}
